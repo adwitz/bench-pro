dff3c6cba1e4889419c8014e0ddd6ff498b6efb6
Adam Witzel <adam.witzel@gmail.com>
2015-08-02 18:39:22 -0700

    Whitespace edit.

--- App/Utils/__tests__/validation-test.js
+++ App/Utils/__tests__/validation-test.js
@@ -1,34 +1,33 @@
 jest.dontMock('../validation');
-describe('weight validation', function(){
+describe('weight validation', function() {
 
   var Validation = require('../validation');
 
-  it('can tell if a weight is greater than or equal to 100', function(){
+  it('can tell if a weight is greater than or equal to 100', function() {
     expect(Validation.weightIsGreaterThanMin(101)).toEqual(true);
   });
 
-  it('can tell if a weight is less than 100', function(){
+  it('can tell if a weight is less than 100', function() {
     expect(Validation.weightIsGreaterThanMin(99)).toEqual(false);
   });
 
-  it('can tell if a weight is less than or equal to 550', function(){
+  it('can tell if a weight is less than or equal to 550', function() {
     expect(Validation.weightIsLessThanMax(449)).toEqual(true);
   });
 
-  it('can tell if a weight is greater than 550', function(){
+  it('can tell if a weight is greater than 550', function() {
     expect(Validation.weightIsLessThanMax(560)).toEqual(false);
   });
 
-  it('can tell if a weight is within range', function(){
+  it('can tell if a weight is within range', function() {
     expect(Validation.weightIsWithinRange(150)).toEqual(true);
   });
 
-  it('it rejects a weight that is out of bounds (greater)', function(){
+  it('it rejects a weight that is out of bounds (greater)', function() {
     expect(Validation.weightIsWithinRange(551)).toEqual(false);
   });
 
-  it('it rejects a weight that is out of bounds (less than)', function(){
+  it('it rejects a weight that is out of bounds (less than)', function() {
     expect(Validation.weightIsWithinRange(99)).toEqual(false);
   });
 });
-
--- App/Utils/validation.js
+++ App/Utils/validation.js
@@ -1,42 +1,42 @@
 'use strict';
 
 var validation = {
-  weightIsDivisbleByFive(weight){
+  weightIsDivisbleByFive(weight) {
     return weight % 5 === 0 ? true : false;
   },
-  weightIsWithinRange(weight){
+  weightIsWithinRange(weight) {
     return this.weightIsGreaterThanMin(weight) && this.weightIsLessThanMax(weight);
   },
-  weightIsGreaterThanMin(weight){
+  weightIsGreaterThanMin(weight) {
     return weight >= 100 ? true : false;
   },
-  weightIsLessThanMax(weight){
+  weightIsLessThanMax(weight) {
     return weight <= 550 ? true : false;
   },
-  weightIsValid(weight){
+  weightIsValid(weight) {
     weight = Number(weight.trim());
-    if (this.weightIsDivisbleByFive(weight) && this.weightIsWithinRange(weight)){
+    if (this.weightIsDivisbleByFive(weight) && this.weightIsWithinRange(weight)) {
       return true;
     }
     return false;
   },
-  getErrorMessage(weight){
+  getErrorMessage(weight) {
     weight = Number(weight.trim());
     var messages = [];
-    if (!this.weightIsGreaterThanMin(weight)){
+    if (!this.weightIsGreaterThanMin(weight)) {
       messages.push('greater than 100lbs');
     }
-    if (!this.weightIsLessThanMax(weight)){
+    if (!this.weightIsLessThanMax(weight)) {
       messages.push('less than 550lbs');
     }
-    if (!this.weightIsDivisbleByFive(weight)){
+    if (!this.weightIsDivisbleByFive(weight)) {
       messages.push('divisible by 5');
     }
     return this.buildErrorString(messages);
   },
-  buildErrorString(messages){
+  buildErrorString(messages) {
     var message = 'Weight must be';
-    if (messages.length > 1){
+    if (messages.length > 1) {
       message = `${message} ${messages[0]} and ${messages[1]}.`;
     } else {
       message = `${message} ${messages[0]}.`;
